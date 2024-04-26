71ea64cfaa11a4eb85d56a6ceef378fcee653932
Madhulika Singh <MadhulikaSingh02@gmail.com>
2024-04-25 17:17:45 -0700

    Counter implementation

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(3);
+
+  function incrementValue() {
+    //Increment Value by 2 to understand update function
+    console.log("Inside incrementValue", counter);
+    if (counter < 10) {
+      setCounter((counter) => counter + 1);
+      //setCounter((counter) => counter + 1);
+    } else {
+      setCounter(3);
+    }
+  }
+  const decrementValue = () => {
+    console.log("Inside decrementValue", counter);
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(3);
+    }
+  };
   return (
     <>
-      <h1>React using Vite as the Bundler</h1>
+      <h1>Counter {counter}</h1>
+      <button type="button" onClick={incrementValue}>
+        Increment-{counter}
+      </button>
+      <button type="button" onClick={decrementValue}>
+        Decrement-{counter}
+      </button>
     </>
   );
 }
